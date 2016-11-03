242f4d5233bfbe4c70a888a218bb21cb2208ee84
Mattr4321 <Mattr4321@gmail.com>
2016-11-03 18:00:02 +0000

    4.2 database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102024031) do
+ActiveRecord::Schema.define(version: 20161103174552) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161102024031) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
   end
 
 end

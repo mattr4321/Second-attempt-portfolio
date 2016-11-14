7477f4d85d9eb1996262a01c4b95595b988d5635
Mattr4321 <Mattr4321@gmail.com>
2016-11-14 19:27:44 +0000

    4.5 revision2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,10 +70,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :colour, :price)
     end
 end
 
-def product_params
-  params.require(:product).permit(:name, :description, :image_url, :colour, :price)
-end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,9 @@ class StaticPagesController < ApplicationController
 
   def landing_page 
     @featured_product = Product.first
-  end
-  
-  def landing_page
     @products = Product.limit(3)
   end
+  
+
   
 end

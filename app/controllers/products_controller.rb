2df3877fc8de3a36d0cd3b8ad1b83fbe92486e47
Mattr4321 <Mattr4321@gmail.com>
2016-11-14 16:29:26 +0000

    4.4 revision

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
+  layout "static"
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
   # GET /products.json
   def index
     @products = Product.all
+    render layout: "products"
     
   end
 
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -1,2 +1,6 @@
 json.extract! product, :id, :name, :description, :image_url, :created_at, :updated_at
 json.url product_url(product, format: :json)
+
+
+
+<% render @products %>

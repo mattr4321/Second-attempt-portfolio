7c6cca46e34f421a216b9c242ecb01effc10eb8a
Mattr4321 <Mattr4321@gmail.com>
2016-11-24 01:13:16 +0000

    5.1 adding routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,14 @@ class OrdersController < ApplicationController
   end
 
   def destroy
+    respond_to do |format|
+      if @product.destroy
+        format.html { redirect_to @product, notice: 'Product was successfully destroyed.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
-
 end

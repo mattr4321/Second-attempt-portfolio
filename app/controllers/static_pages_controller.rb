3569693d5cd4c1fd84767094671ec81f3e9afdef
Mattr4321 <Mattr4321@gmail.com>
2016-11-30 13:42:21 +0000

    5.3 mailer submission

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,16 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
   
+  
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'mattr4321@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
 
   
 end

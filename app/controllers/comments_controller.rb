aa794d03fe7a30690234bfa3399266286471299b
Mattr4321 <Mattr4321@gmail.com>
2016-12-28 02:59:21 +0000

    5.9 resubmission with controller issues addressed. Comment issues still there

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,19 @@
 class CommentsController < ApplicationController
-Post.where(:published => true).paginate(:page => params[:page]).order('id DESC')
-
     def create
         @product = Product.find(params[:product_id])
         @comment = @product.comments.new(comment_params)
         @posts = Post.paginate(:page => params[:page])
         @comment.user = current_user
         respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-        @comment.save
-        redirect_to product_path(@product)
-     end
+          if @comment.save
+            format.html { redirect_to @product, notice: 'Review was created successfully.' }
+            format.json { render :show, status: :created, location: @product }
+          else
+            format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+            format.json { render json: @comment.errors, status: :unprocessable_entity }
+          end
+        end
+    end
 
     def destroy
     end
@@ -26,6 +23,5 @@ Post.where(:published => true).paginate(:page => params[:page]).order('id DESC')
     private 
     def comment_params
         params.require(:comment).permit(:user_id, :body, :rating)
-    end
     end
 end

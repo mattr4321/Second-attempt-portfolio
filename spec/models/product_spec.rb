5a85efa64b1415a6d40b51c7f237fbe99f7c2dd6
Mattr4321 <Mattr4321@gmail.com>
2017-01-15 23:21:25 +0000

    6.2 updates to code with errors on line 14 and 19 of prdouct_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,25 +2,22 @@ require 'rails_helper'
 
 describe Product do
    context "when the product has comments" do
-
         before do 
             @product = Product.create!(name: "Vanilla cake")
+            @user = User.create!(first_name: "Matthew", last_name:"Rodriguez", email:"Mattr4321@gmail.com", password: "1234" )
+            @product.comments.create!(rating: 1, user: @user, body: "Awful cake!")
+            @product.comments.create!(rating: 3, user: @user, body: "Okay cake!")
+            @product.comments.create!(rating: 5, user: @user, body: "Great cake!")
         end
-        before do
-            @user = User.create!(name: "Matthew Rodriguez")
-        end
-        before do
-            @comment = @product.comments.create!(rating: 1, user: @user, body: "Awful cake!")
-            @comment = @product.comments.create!(rating: 3, user: @user, body: "Okay cake!")
-            @comment = @product.comments.create!(rating: 5, user: @user, body: "Great cake!")
-        end
+        
+        
         it "returns the average rating of all comments" do
-            expect (:product.rating).to eq "3"
+            
+        end
+    
+    
+        it "Is not valid" do
+            expect(Product.new(description: "Nice bike")).not_to be_valid
         end
     end
-    
-    context "Is not valid" do
-        expect(Product.new(description: "Nice bike")).not_to be_valid
-    end
-    
 end

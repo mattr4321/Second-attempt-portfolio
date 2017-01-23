61ba38b2100b9c0fbacabe8dd0a6d21c62a19940
Mattr4321 <Mattr4321@gmail.com>
2017-01-23 00:53:50 +0000

    6.14 active job

--- app/assets/javascripts/channels/product_channel.rb
+++ app/assets/javascripts/channels/product_channel.rb
@@ -2,11 +2,19 @@
  class ProductChannel < ApplicationCable::Channel
    def subscribed
      # stream_from "some_channel"
-     stream_from "product_channel"
+     #stream_from "product_channel"
+     
    end
  
    def unsubscribed
      # Any cleanup needed when channel is unsubscribed
    end
+   
+   def listen
+       stop_all_streams
+       stream_for data["product_id"]
+       
+   end
+   
  end
  #changes added for 6.13

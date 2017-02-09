cd7cf0d0574b35cf69a06f7ab9c61574cc88c71a
Mattr4321 <Mattr4321@gmail.com>
2017-02-09 15:29:22 +0000

    styling edits and changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.app_generators.javascript_engine = :javascript 
   
   config.action_mailer.default_url_options = { :host => 'localhost', port: 8080 }
   
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.web_socket_server_url = "wss://kellyscakes.herokuapp.com/cable"
+config.web_socket_server_url = "wss://kellyscakes.herokuapp.com/cable"
 config.action_cable.allowed_request_origins = ['https://kellyscakes.herokuapp.com', 'http://kellyscakes.herokuapp.com']
   
 end

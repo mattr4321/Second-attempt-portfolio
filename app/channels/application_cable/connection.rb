d6a629ba289754773febdea654ad7ed07f665fa8
Mattr4321 <Mattr4321@gmail.com>
2017-02-10 17:51:33 +0000

    action cable fix with cable.js added

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,9 +2,9 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
   	  identified_by :current_user
   	
-  	def connect
-  		  self.current_user = find_verified_user
-    end
+  	  def connect
+  		    self.current_user = find_verified_user
+          end
     
 
     def disconnect
